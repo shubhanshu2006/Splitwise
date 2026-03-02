ec98f42e01ec86cb7375a41fccacfe546fc2af95
Ujjwal Kesarwani <ujjwalkes19@gmail.com>
2026-03-02 19:12:54 +0530

    feat: add ForgotPasswordRequest and ResetPasswordRequest types to user model

--- backend/models/user.go
+++ backend/models/user.go
@@ -1,9 +1,11 @@
 package models
+
 import (
 	"time"
 
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
+
 type User struct {
 	ID        primitive.ObjectID `bson:"_id,omitempty" json:"id"`
 	Name      string             `bson:"name"          json:"name"`
@@ -17,11 +19,27 @@ type RegisterRequest struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
 }
-	type LoginRequest struct {
+type LoginRequest struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
 }
-	type UpdateProfileRequest struct {
+type UpdateProfileRequest struct {
 	Name string `json:"name"`
 }
 
+type ForgotPasswordRequest struct {
+	Email string `json:"email"`
+}
+
+type ResetPasswordRequest struct {
+	Token       string `json:"token"`
+	NewPassword string `json:"new_password"`
+}
+
+type PasswordReset struct {
+	ID        primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	UserID    primitive.ObjectID `bson:"user_id"       json:"user_id"`
+	Token     string             `bson:"token"         json:"token"`
+	ExpiresAt time.Time          `bson:"expires_at"    json:"expires_at"`
+	CreatedAt time.Time          `bson:"created_at"    json:"created_at"`
+}
